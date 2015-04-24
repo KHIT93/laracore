5796ea303cde5b77a004b2a318ee0b1f23a7ab85
Kenneth <kenneth@khansen-it.dk>
2015-04-24 11:48:20 +0200

    Seperated dynamic sidebar generation into another view file for future use.

--- resources/views/admin/partials/_dynamic_sidebar.blade.php
+++ resources/views/admin/partials/_dynamic_sidebar.blade.php
@@ -1,4 +1,4 @@
-<ul class="nav nav-sidebar">
+<!-- <ul class="nav nav-sidebar">
     <li @if(Request::is('admin/dashboard') || Request::is('admin')) class="active" @endif><a href="/admin/dashboard"><i class="fa fa-tachometer"></i> Overview <span class="sr-only">(current)</span></a></li>
     <li @if(Request::is('admin/content')) class="active" @endif><a href="/admin/content"><i class="fa fa-file-o"></i> Content</a></li>
     <li @if(Request::is('admin/layout')) class="active" @endif>
@@ -15,4 +15,22 @@
     <li @if(Request::is('admin/users') || Request::is('admin/users/*')) class="active" @endif><a href="/admin/users"><i class="fa fa-user"></i> Users</a></li>
     <li @if(Request::is('admin/config') || Request::is('admin/config/*')) class="active" @endif><a href="/admin/config"><i class="fa fa-cog"></i> Configuration</a></li>
     <li @if(Request::is('admin/reports') || Request::is('admin/reports/*')) class="active" @endif><a href="/admin/reports"><i class="fa fa-book"></i> Reports</a></li>
+</ul> -->
+@if(count($items))
+<ul class="nav nav-sidebar">
+    @foreach ($items as $item)
+    <li @if(Request::is($item->link)) class="active" @endif>
+        <a href="{{ url($item->link) }}">{!! FA::icon($item->icon) !!} {{ $item->name }}</a>
+        @if(count($item->childs))
+        <ul class="dropdown-menu" role="menu">
+            @foreach($item->childs as $child)
+            <li @if(Request::is($child->link)) class="active" @endif>
+                <a href="{{ url($child->link) }}">{!! FA::icon($child->icon) !!} {{ $child->name }}</a>
+            </li>
+            @endforeach
+        </ul>
+        @endif
+    </li>
+    @endforeach
 </ul>
+@endif
