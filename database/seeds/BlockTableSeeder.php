9a96d6994c2ce13a01fccf0382671a26f8482037
Kenneth Hansen <kenneth@khansen-it.dk>
2015-07-13 02:07:56 +0200

    Added missing seeder for blocks table

---
+++ database/seeds/BlockTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Block;
+
+class BlockTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Block::create(['module' => 'system', 'delta' => 'main', 'theme' => 'default', 'position' => 0, 'section' => 'content', 'visibility' => 1, 'pages' => '', 'title' => '', 'description' => 'Primary content', 'body' => '']);
+
+    }
+}
