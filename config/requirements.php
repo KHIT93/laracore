6a3545e8eb995084ef94173f4fad9e8b4969be5d
Kenneth Hansen <kenneth@khansen-it.dk>
2015-07-18 19:33:00 +0200

    Add config for requirements

---
+++ config/requirements.php
@@ -0,0 +1,39 @@
+<?php
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Folders Permissions
+    |--------------------------------------------------------------------------
+    |
+    | These are the default Laracore folders permissions, that are required
+    | for the application to work properly
+    |
+    */
+
+    'permissions' => [
+        'storage/app/'          => 775,
+        'storage/framework/'    => 775,
+        'storage/logs/'         => 775,
+        'bootstrap/cache/'      => 775,
+        'public/themes/'        => 775
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Server Requirements
+    |--------------------------------------------------------------------------
+    |
+    | This is the default Laracore server requirements, we check if the extension
+    | is enabled by looping through the array and run "extension_loaded" on it.
+    |
+    */
+    'extensions' => [
+        'openssl',
+        'mcrypt',
+        'pdo',
+        'mbstring',
+        'tokenizer'
+    ],
+    'php_version' => '5.5.9'
+];
